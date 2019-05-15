0fb86bae88f9c78e31b331500142917f180245af
Sohieeb <ibrahim.abdelrahman95@gmail.com>
2019-05-16 07:14:51 +0800

    prepared to deploy to heroku

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// // public folder to store assets
+// app.use(express.static(__dirname + '/public'));
+
+// routes for app
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+// listen on port 8000 (for localhost) or the port  defined for heroku
+var port = process.env.PORT || 8000;
+app.listen(port);
