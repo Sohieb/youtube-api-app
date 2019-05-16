0a9bdac1937ad97c9b6533b99524698bfc7b2dc4
Sohieeb <ibrahim.abdelrahman95@gmail.com>
2019-05-16 23:41:10 +0800

    fix the api call to use the channel id after getting it from the search api

--- public/main.js
+++ public/main.js
@@ -90,7 +90,40 @@ function getChannel(channel) {
         q: channel
     }).then(res => {
         console.log(res);
-    });
+        console.log(res.result.items[0].id.channelId);
+        const channelId = res.result.items[0].id.channelId;
+        gapi.client.youtube.channels.list({
+            part: 'snippet,contentDetails,statistics',
+            id: channelId
+        }).then(response => {
+            // console.log(response);
+            const channel = response.result.items[0];
+            // console.log(channel);
+            const output = `
+                <ul class="collection">
+                    <li class="collection-item">Title: 
+                    ${channel.snippet.title}</li>
+                    <li class="collection-item">ID: 
+                    ${channel.id}</li>
+                    <li class="collection-item">Subscribers: 
+                    ${numberWithCommas(channel.statistics.subscriberCount)}</li>
+                    <li class="collection-item">Views: 
+                    ${numberWithCommas(channel.statistics.viewCount)}</li>
+                    <li class="collection-item">Videos: 
+                    ${numberWithCommas(channel.statistics.videoCount)}</li>
+                </ul>
+                <p>${channel.snippet.description}</p>
+                <hr>
+                <a class="btn grey darken-2" target="_blank" 
+                href="https://youtube.com/${channel.snippet.customUrl}">Visit Channel</a>
+            `;
+            showChannelData(output);
+
+            const playlistId = channel.contentDetails.relatedPlaylists.uploads;
+            requestVideoPlaylist(playlistId);
+        });
+    }).catch(err => alert('No Channel By That Name'));
+    return;
     ///////
     gapi.client.youtube.channels.list({
         part: 'snippet,contentDetails,statistics',
