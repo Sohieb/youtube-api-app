928f0dabcf882710ce13359f33229a5f19d902c4
Sohieeb <ibrahim.abdelrahman95@gmail.com>
2019-05-16 23:26:59 +0800

    trying out search api instead

--- public/main.js
+++ public/main.js
@@ -88,9 +88,35 @@ function getChannel(channel) {
         part: 'snippet',
         type: 'channel',
         q: channel
-    }).then(res => {
-        console.log(res);
-    });
+    }).then(response => {
+        // console.log(response);
+        const channel = response.result.items[0];
+        // console.log(channel);
+        const output = `
+            <ul class="collection">
+                <li class="collection-item">Title: 
+                ${channel.snippet.title}</li>
+                <li class="collection-item">ID: 
+                ${channel.id}</li>
+                <li class="collection-item">Subscribers: 
+                ${numberWithCommas(channel.statistics.subscriberCount)}</li>
+                <li class="collection-item">Views: 
+                ${numberWithCommas(channel.statistics.viewCount)}</li>
+                <li class="collection-item">Videos: 
+                ${numberWithCommas(channel.statistics.videoCount)}</li>
+            </ul>
+            <p>${channel.snippet.description}</p>
+            <hr>
+            <a class="btn grey darken-2" target="_blank" 
+            href="https://youtube.com/${channel.snippet.customUrl}">Visit Channel</a>
+        `;
+        showChannelData(output);
+
+        const playlistId = channel.contentDetails.relatedPlaylists.uploads;
+        requestVideoPlaylist(playlistId);
+    })
+        .catch(err => alert('No Channel By That Name'));
+    return;
     ///////
     gapi.client.youtube.channels.list({
         part: 'snippet,contentDetails,statistics',
